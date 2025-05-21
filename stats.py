ee43deff717f8501e51a964ada2c169651ddf3b5
MrFaultyT <wanye787@gmail.com>
2025-05-21 22:41:18 +0100

    further updating to change position of making all letters lowercase, still getting an extra 2 e's

--- stats.py
+++ stats.py
@@ -5,10 +5,11 @@ def get_num_words(book_string):
 
 def get_num_character(book_string):
     string_count = {}
-    book_string = book_string.lower()
+    #book_string = book_string.lower()
     for s in book_string:
         ### the isalpha added as I had extra 2 in moby dick - hopefully this corrects it
         if s.isalpha(): 
+            s = s.lower()
             if s in string_count:
                 string_count[s] += 1
             else:
