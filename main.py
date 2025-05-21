f10a8fbe555ecbb4ed12ff55cdfeb8faa09af5a9
MrFaultyT <wanye787@gmail.com>
2025-05-21 12:42:55 +0100

    updated code to print number of words instead of text

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def get_book_text(file_path):
 
 def main():
     book_string = get_book_text("books/frankenstein.txt")
-    print(book_string)
+    words = book_string.split()
+    count = len(words)
+    print(f"{count} words found in the document")
 
 main()
 
