7ac590da122d1a9a669393f93e1b99333a156060
MrFaultyT <wanye787@gmail.com>
2025-05-21 13:14:53 +0100

    updated both the main.py and stats.py to function together, main.py importants the text as strings, stats counts them and main puts it all together

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
-def main():
-    from stats import get_num_words
-main()
+from stats import get_num_words
 
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    count = get_num_words(file_contents)
+    print(f"{count} words found in the document")
+
+main()
--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
-def get_num_words():
-    book_string = get_book_text("books/frankenstein.txt")
+def get_num_words(book_string):
     words = book_string.split()
     count = len(words)
-    print(f"{count} words found in the document")
+    return count 
+
